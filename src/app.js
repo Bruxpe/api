887bf82458276a8e82868bd6539f7361f6648344
Bruxpe <bruno.henry.1chal@gmail.com>
2023-05-06 19:43:09 -0500

    ahora

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev')); // usamos morgan  //!
 server.use((req, res, next) => {
-  console.log("podra ser x2  app :v");
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
